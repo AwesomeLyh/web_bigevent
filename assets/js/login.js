e83146df4bac8ab672c16c40f891307ad6bde4da
AwesomeLi <AwesomeLiwork@163.com>
2020-10-06 16:54:19 +0800

    表单验证完成

--- assets/js/login.js
+++ assets/js/login.js
@@ -8,4 +8,13 @@ $(function () {
         $("#reg-box").show();
         $("#login-box").hide();
     });
+
+    var form = layui.form
+    form.verify({
+        pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+        repwd: function (val) {
+            let pwd = $("#reg-box [name=password]").val()
+            if (pwd !== val) return "两次密码不一致"
+        }
+    })
 })
