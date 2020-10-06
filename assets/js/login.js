29d1827259bc05db885eba3b00d51af0a215354e
AwesomeLi <AwesomeLiwork@163.com>
2020-10-06 15:50:04 +0800

    登陆注册页面结构搭建完成

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,11 +1,11 @@
 $(function () {
     // 点击且换登录预注册
     $("#reg-btn").on("click", function () {
-        $("#reg").hide();
-        $("#login").show();
+        $("#reg-box").hide();
+        $("#login-box").show();
     });
     $("#login-btn").on("click", function () {
-        $("#reg").show();
-        $("#login").hide();
+        $("#reg-box").show();
+        $("#login-box").hide();
     });
 })
