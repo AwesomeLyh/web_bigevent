6f02ff7fff75352bf418008f054cf160957e05da
AwesomeLi <AwesomeLiwork@163.com>
2020-10-08 18:51:06 +0800

    完成权限认证

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,5 +1,19 @@
 $(function () {
     $.ajaxPrefilter(function (option) {
         option.url = "http://ajax.frontend.itheima.net" + option.url;
+        // 在BaseApi 全局挂载对调用权限接口的判断，给请求加上headers
+        if (option.url.indexOf("/my/") !== -1) {
+            option.headers = {
+                Authorization: localStorage.getItem('token') || ''
+            }
+        }
+        // 在BaseApi 全局挂载对未登录跳转的校验
+        // complete无论服务器返回结果如何 都会执行方法体
+        option.complete = function (res) {
+            if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+                localStorage.removeItem("token");
+                location.href = "/Big/login.html";
+            }
+        }
     })
 })
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,16 @@
 $(function () {
+    var layer = layui.layer;
     getUserinfo();
     // 获取登录的用户信息
     function getUserinfo() {
-        const token = localStorage.getItem("token");
+        // const token = localStorage.getItem("token");
         $.ajax({
             type: "get",
             url: "/my/userinfo",
-            headers: {
-                "Authorization": token || ""
-            },
+            // 在BaseApi 全局挂载了判断，对调用权限接口的请求加上headers
+            // headers: {
+            //     "Authorization": token || ""
+            // },
             success: function (response) {
                 if (response.status !== 0) return layer.msg(response.message);
                 renderUserData(response.data);
@@ -31,4 +33,22 @@ $(function () {
             $(".layui-nav-img").hide();
         }
     }
+
+    //清除本地存储中的Token
+    function clearToken() {
+        localStorage.removeItem("token")
+    }
+
+
+    //退出登录
+    $("#exit").on("click", function (e) {
+        console.log(111);
+        e.preventDefault();
+        layer.confirm('确认退出？', { icon: 3, title: '提示' }, function (index) {
+            clearToken();
+            location.href = "/Big/login.html";
+            // 关闭 confirm 询问框
+            layer.close(index)
+        });
+    });
 })
