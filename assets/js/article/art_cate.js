cf0617c3a33972209017cf4359c0d55f7c48146f
AwesomeLi <AwesomeLiwork@163.com>
2020-10-10 10:24:37 +0800

    完成文章分类删除修改功能

--- assets/js/article/art_cate.js
+++ assets/js/article/art_cate.js
@@ -1,7 +1,22 @@
 $(function () {
-    var layer = layui.layer
+    var layer = layui.layer;
+    var form = layui.form;
     var indexAdd = null;
 
+    initArtCateList();
+    // 获取文章分类
+    function initArtCateList() {
+        $.ajax({
+            type: "GET",
+            url: "/my/article/cates",
+            success: function (response) {
+                var htmlStr = template('tpl-table', response);
+                $('tbody').html(htmlStr);
+            }
+        });
+    }
+
+    //添加按钮弹出dialog
     $("#btnAddCate").on("click", function () {
         indexAdd = layer.open({
             type: 1,
@@ -11,6 +26,7 @@ $(function () {
         })
     });
 
+    //提交添加表单
     $("body").on("submit", "#form-add", function (e) {
         e.preventDefault();
         $.ajax({
@@ -26,20 +42,56 @@ $(function () {
             }
         })
     })
-
-    initArtCateList();
-    // 获取文章分类
-    function initArtCateList() {
+    //修改按钮弹出dialog
+    var indexEdit = null;
+    $("tbody").on("click", "#btn-edit", function (e) {
+        var art_id = $(this).attr("data-id");
+        indexEdit = layer.open({
+            type: 1,
+            area: ['500px', '250px'],
+            title: '编辑分类信息',
+            content: $('#dialog-edit').html()
+        });
         $.ajax({
             type: "GET",
-            url: "/my/article/cates",
+            url: "/my/article/cates/" + art_id,
             success: function (response) {
-                var htmlStr = template('tpl-table', response);
-                console.log(htmlStr);
-                $('tbody').html(htmlStr);
+                console.log(response.data);
+                form.val("form-edit", response.data);
             }
         });
-    }
+    });
 
+    //提交添加表单
+    $("body").on("submit", "#form-edit", function (e) {
+        e.preventDefault()
+        console.log(1111);
+        $.ajax({
+            type: "POST",
+            url: "/my/article/updatecate",
+            data: form.val("form-edit"),
+            success: function (response) {
+                if (response.status !== 0) return layer.msg(response.message);
+                layer.msg("修改成功");
+                layer.close(indexEdit);
+                initArtCateList();
+            }
+        });
+    })
+
+    $("tbody").on("click", "#btn-del", function (e) {
+        var art_id = $(this).attr("data-id");
+        layer.confirm('确认删除？', { icon: 3, title: '提示' }, function (index) {
+            $.ajax({
+                type: "GET",
+                url: "/my/article/deletecate/" + art_id,
+                success: function (response) {
+                    layer.msg("删除成功");
+                    initArtCateList();
+                }
+            });
+            layer.close(index);
+        });
+    })
 
 })
